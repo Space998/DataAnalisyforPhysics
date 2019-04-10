86146d10afa0a349d34437bef3b0e1134dcca733
Space998 <davide.rolino@gmail.com>
2019-04-10 23:49:23 +0200

    Creation of the class -Data- with the overloading of the basic operations, this as to be completed with literal calculation and significant figures

--- AnalisiDati.py
+++ AnalisiDati.py
@@ -1,3 +1,4 @@
+from data import Data
 import sys
 
 if len(sys.argv) != 2:
@@ -5,4 +6,18 @@ if len(sys.argv) != 2:
     exit()
 
 print ("Nome del file:",sys.argv[1])
-print ("Test")    
+
+#test data.py class
+d = Data(12, 5, "A", "m")
+print (d)
+e = Data(11, 4, "A", "m")
+print(e)
+print("Somma: ", d + e)
+print("Sottrazione: ", d - e)
+print("Moltiplicazione: ", d * e)
+print("Divisione: ", d/e)
+#print(2/4)
+
+f = Data(10, 4, "B", "m")
+print(f)
+print(d+f)
