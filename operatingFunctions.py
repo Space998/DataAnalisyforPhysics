e5702acf0ddfdf7be667b910b1c12c4715eaeb0a
Space998 <davide.rolino@gmail.com>
2019-10-10 23:22:32 +0200

    Complited the function to evaluate custom mathematical functions applied to the data set

--- operatingFunctions.py
+++ operatingFunctions.py
@@ -1,5 +1,10 @@
 from dataList import DList
 from dataSet import DSet
+from sympy import *
+import numpy
+import math
+#from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application, parse_expr
+#init_printing(use_unicode=True)
 
 dataSet_dict = {}
 
@@ -28,9 +33,60 @@ def printData(set):
                 print(dataSet._set[i]._list[j],_value, '\t', dataSet._set[i]._list[j],_error, end = '\t')
         print()
 
-def function(expr):
-    expr = " ".join(expr)
-    print(expr)
+def startAnalisy(null):
+    for i in dataSet_dict:
+        for j in dataSet_dict[i]._set:
+            j._name = Symbol(j._name)
 
-functionDict = {'FUNCTION': function, 'NEWDATA': newData, 'PRINTDATA': printData}
+def function(expr):
+    #select the correct data set
+    dataSet = dataSet_dict[expr[0]] 
+    #create a new DList to contain the new calculated value
+    newDList = DList(expr[1], 'user')
+    #collect the math function
+    expr = expr[2:]
+    expr = " ".join(expr)
+    #print(expr)
+    #define two list to contain the variables in wich the calculation will be made
+    variable = []
+    errVariable = []
+    #creates the mathematical expression for the derivates -> this formula is the differential formula for error propagation
+    expr1 = 'sqrt('
+    for i in dataSet_dict:
+        for j in dataSet_dict[i]._set:
+            if str(j._name) in expr:
+                expr1 = expr1 + '((' + str(diff(expr, j._name)) + ')' + '*(' + str(j._name) + '_error)**2)' + ' + '
+                variable.append(j._name)
+                errVariable.append(j._name)
+                errVariable.append(str(j._name) + '_error')
+    expr1 = ' '.join(expr1.split(' ')[:-2]) + ')'
+    #creates the solvable expressions for sympy using numpy   
+    variable_dict = {}
+    errVariable_dict = {}
+    for j in range(len(dataSet._set[0]._list)):
+        for i in variable:
+            variable_dict[i] = i._list[j]._value
+        for i in errVariable:
+            if '-_error' not in i:
+                errVariable_dict[i] = i._list[j]._value
+            else: 
+                val = i.split('_')[0]
+                errVariable_dict[i] = val._list[j]._error
+        newDList._list.append(Data(expr.evalf(subs=variable_dict),expr1.evalf(subs=errVariable_dict)))
+    '''
+    print(expr1)
+    print(variable)
+    print(variable_dict)
+    print(errVariable) 
+    print(errVariable_dict)
+    func = lambdify(variable, expr, "numpy") 
+    funcErr = lambdify(errVariable, expr1, "numpy")
+    for i in range(len(variable)):
+        for i in range(len(dataSet._set[0]._list)):
+            newDList._list.append(Data(func(variable[i]._list[j]._value)))
+    '''
+        
+
+
+functionDict = {'FUNCTION': function, 'NEWDATA': newData, 'PRINTDATA': printData, 'STARTANALISY': startAnalisy}
 #add ability to create custom dataSets
