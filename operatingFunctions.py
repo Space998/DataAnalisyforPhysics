dd0f89022eaccc0f2ea56ab59eacef1ab9b4d813
Space998 <davide.rolino@gmail.com>
2019-10-12 00:36:51 +0200

    Finisched function function in operatingFunctions. Need to debug PRINTDATA; row and colums are inverted

--- operatingFunctions.py
+++ operatingFunctions.py
@@ -5,6 +5,7 @@ from sympy import *
 import numpy
 import math
 import genericFunctions
+from sympy.parsing.sympy_parser import parse_expr
 #from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application, parse_expr
 #init_printing(use_unicode=True)
 
@@ -37,7 +38,7 @@ def newData(data):
         #print(dataSet._set[i]._name)  
     for i in range(len(data[1:])):
         value = data[i+1].split()
-        print('--',value)
+        #print('--',value)
         #for j in range(len(dataSet._set)):
         #    print('--',j)
         for n in range(0,len(value),2):
@@ -65,13 +66,13 @@ def printData(set):
 def startAnalisy(null):
     for i in dataSet_dict:
         for j in dataSet_dict[i]._set:
-            j._name = Symbol(j._name)
+            Symbol(j._name +'_s')
 
 def function(expr):
     #select the correct data set
     dataSet = dataSet_dict[expr[0]] 
     #create a new DList to contain the new calculated value
-    newDList = DList(expr[1], 'user')
+    newDList = DList(expr[1])
     #collect the math function
     expr = expr[2:]
     expr = " ".join(expr)
@@ -83,25 +84,35 @@ def function(expr):
     expr1 = 'sqrt('
     for i in dataSet_dict:
         for j in dataSet_dict[i]._set:
-            if str(j._name) in expr:
-                expr1 = expr1 + '((' + str(diff(expr, j._name)) + ')' + '*(' + str(j._name) + '_error)**2)' + ' + '
-                variable.append(j._name)
-                errVariable.append(j._name)
-                errVariable.append(str(j._name) + '_error')
+            if str(j._name+'_s') in expr:
+                expr1 = expr1 + '((' + str(diff(expr, j._name+'_s')) + ')' + '*(' + str(j._name+'_s') + '_error)**2)' + ' + '
+                variable.append(j._name+'_s')
+                errVariable.append(j._name+'_s')
+                errVariable.append(str(j._name+'_s') + '_error')
     expr1 = ' '.join(expr1.split(' ')[:-2]) + ')'
+    #print(expr)
+    #print(expr1)
     #creates the solvable expressions for sympy using numpy   
     variable_dict = {}
     errVariable_dict = {}
+    func = parse_expr(expr)
+    func1 = parse_expr(expr1)
     for j in range(len(dataSet._set[0]._list)):
         for i in variable:
-            variable_dict[i] = i._list[j]._value
+            variable_dict[i] = dataSet._set[0]._list[j]._value
         for i in errVariable:
-            if '-_error' not in i:
-                errVariable_dict[i] = i._list[j]._value
+            if '_error' not in i:
+                errVariable_dict[i] = dataSet._set[0]._list[j]._value
             else: 
-                val = i.split('_')[0]
-                errVariable_dict[i] = val._list[j]._error
-        newDList._list.append(Data(expr.evalf(subs=variable_dict),expr1.evalf(subs=errVariable_dict)))
+                errVariable_dict[i] = dataSet._set[0]._list[j]._error
+        #print(func.evalf(subs=variable_dict))
+        #print(func1)
+        #print(func1.evalf(subs=errVariable_dict))
+        newDList._list.append(Data(func.evalf(subs=variable_dict),func1.evalf(subs=errVariable_dict)))
+    #print(variable_dict)
+    #print(errVariable_dict)
+    #print(newDList._list)
+    print(newDList._list)
     dataSet._set.append(newDList)
     ''' 
     print(expr1)
