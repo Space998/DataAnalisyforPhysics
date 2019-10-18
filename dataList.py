51b7f17ba970baaf39f622878bea228132a9a5d2
Space998 <davide.rolino@gmail.com>
2019-10-18 15:28:32 +0200

    Sobstitution of the python Lists of the Dlist class with numpy arrays, found bug with multilines comments

--- dataList.py
+++ dataList.py
@@ -4,12 +4,13 @@ Author: Davide Rolino
 Class for list of data.
 """
 
+import numpy as np
 class DList(object):
-    def __init__(self, name, unit='user'):
+    def __init__(self, name, unit=''):
         self._name = name
         self._unit = unit #defite the mesure unit of the list of data
-        self._valueList = []
-        self._errorList = []
+        self._valueList = np.array([])
+        self._errorList = np.array([])
         
 #methods to get class' parameters
     def get_Name(self):
@@ -18,11 +19,12 @@ class DList(object):
     def get_Unit(self):
         return self._unit
 
-    def get_List(self):
-        return self._list
-
+    #def get_List(self):
+        #return self._list
+'''
 #print method for the list
     def Print(self):
         print(self._name, '\t\u03C3Err(', self._name, ')') 
         for i in range(len(self._list)):
             print(self._list[i]._value, '\t', self._list)
+            '''
--- operatingFunctions.py
+++ operatingFunctions.py
@@ -32,29 +32,39 @@ def operation(file):
                     break
             #match = re.findall("NEWDATA([\s\w]*?)END", file)
             #print(match)
+        elif line[0] == 'PLOT':
+            #print('--Hello')
+            for j in range(i,len(file)):
+                newLine = file[j].split()
+                if newLine[0] == 'END':
+                    info = file[i:j]
+                    newPlot(info)
+                    break
 
 def newData(data):
     names = data[0].split()
     if names[1] not in dataSet_dict:
         dataSet_dict[names[1]] = DSet(names[1])
     dataSet = dataSet_dict[names[1]]
+    num = len(data[1:])
     for i in range(0,len(names[2:]),2):
         dataSet._set.append(DList(names[i+2], names[i+3]))
-        #print(dataSet._set[i]._name)  
-    for i in range(len(data[1:])):
+        #print(dataSet._set[int(i/2)]._name)  
+        dataSet._set[int(i/2)]._valueList = np.zeros(num)
+        dataSet._set[int(i/2)]._errorList = np.zeros(num)
+    for i in range(num):
         value = data[i+1].split()
-        #print('--',value)
+        print('--',value)
         #for j in range(len(dataSet._set)):
         #    print('--',j)
         for n in range(0,len(value),2):
             #print(n, dataSet._set[int(n/2)]._name)
             if value[n+1] == '#':
-                dataSet._set[int(n/2)]._valueList.append(value[n])
-                dataSet._set[int(n/2)]._errorList.append(0)
-
+                dataSet._set[int(n/2)]._valueList[i] = value[n]
             else:
-                dataSet._set[int(n/2)]._valueList.append(value[n])
-                dataSet._set[int(n/2)]._errorList.append(value[n+1])
+                dataSet._set[int(n/2)]._valueList[i] = value[n]
+                dataSet._set[int(n/2)]._errorList[i] = value[n+1]
+    #print(dataSet._set[int(0)]._valueList, dataSet._set[0]._errorList)
 
 def printData(set):
     for j in range(len(set)):
@@ -80,8 +90,11 @@ def startAnalisy(null):
 def function(expr):
     #select the correct data set
     dataSet = dataSet_dict[expr[0]] 
+    num = len(dataSet._set[0]._valueList)
     #create a new DList to contain the new calculated value
     newDList = DList(expr[1])
+    newDList._valueList = np.zeros(num)
+    newDList._errorList = np.zeros(num)
     #collect the math function
     expr = expr[2:]
     expr = " ".join(expr)
@@ -101,14 +114,14 @@ def function(expr):
         for n in variable:
             expr1 = expr1 + '((' + str(diff(expr, n)) + ')' + '*(' + str(n) + '_error)**2)' + ' + '
     expr1 = ' '.join(expr1.split(' ')[:-2]) + ')'
-    #print(expr)
-    #print(expr1)
+    print(expr)
+    print(expr1)
     #creates the solvable expressions for sympy using numpy   
     variable_dict = {}
     errVariable_dict = {}
     func = parse_expr(expr)
     func1 = parse_expr(expr1)
-    for j in range(len(dataSet._set[0]._valueList)):
+    for j in range(num):
         for i in variable:
             variable_dict[i] = dataSet._set[0]._valueList[j]
         for i in errVariable:
@@ -119,8 +132,8 @@ def function(expr):
         #print(func.evalf(subs=variable_dict))
         #print(func1)
         #print(func1.evalf(subs=errVariable_dict))
-        newDList._valueList.append(func.evalf(subs=variable_dict))
-        newDList._errorList.append(func1.evalf(subs=errVariable_dict))
+        newDList._valueList[j] = func.evalf(subs=variable_dict)
+        newDList._errorList[j] = func1.evalf(subs=errVariable_dict)
     #print(variable_dict)
     #print(errVariable_dict)
     #print(newDList._list)
@@ -139,29 +152,52 @@ def function(expr):
             newDList._list.append(Data(func(variable[i]._list[j]._value)))
     '''
         
-def simplePlot(info):
-    dataSet = dataSet_dict[info[0]]
+def newPlot(info):
+    #print(info[0].split()[1])
+    dataSet = dataSet_dict[info[0].split()[1]]
+    #print(dataSet._name)
+    title = info[1]
+    title1 = ''.join(info[1].split())
+    #print(title) 
+    yname = info[3].split()[0] + ' ('
+    ylabel = info[3].split()[1]
+    #print(yname)
     for i in range(len(dataSet._set)):
-        if dataSet._set[i]._name == info[1]:
+        if dataSet._set[i]._name == info[2].split()[1]:
             y = dataSet._set[i]._valueList
-            print(y)
-            yname = dataSet._set[i]._name+' '+dataSet._set[i]._unit
+            yname += dataSet._set[i]._unit
+            yname += ')'
+            #print(y)
+    np.asarray(y)
+    xname = info[2].split()[0] + ' ('
+    #print(xname)
     for i in range(len(dataSet._set)):
-        if dataSet._set[i]._name == info[2]:
+        if dataSet._set[i]._name == info[3].split()[2]:
             x = dataSet._set[i]._valueList
-            print(x)
-            xname = dataSet._set[i]._name+' '+dataSet._set[i]._unit
-    title1 = ' '.join(info[3:])
-    title2 = ''.join(info[3:])
-    fig, ax = plt.subplots()
-    ax.plot(x,y)
-
+            xname += dataSet._set[i]._unit
+            xname += ')'
+            #print(x)
+    x = np.asarray(x)
+    #print('y ',y)
+    #print('x ', x)
+    #title1 = ' '.join(info[3:])
+    #title2 = ''.join(info[3:])
+    plt.plot(x,y,label=ylabel)
+    plt.title(title)
+    plt.xlabel(xname)
+    plt.ylabel(yname)
+    #if len(info) > 3:   
+    plt.grid()
+    plt.legend()
+    plt.savefig(title1)
+    #plt.show()
+    '''
     ax.set(xlabel= xname, ylabel= yname,
         title= title1)
     ax.grid()
 
     fig.savefig(title2+".png")
     plt.show()
-
-functionDict = {'PRINTDATA': printData, 'STARTANALISY': startAnalisy, 'FUNCTION': function, 'SIMPLEPLOT': simplePlot}
+    '''
+functionDict = {'PRINTDATA': printData, 'STARTANALISY': startAnalisy, 'FUNCTION': function}
 #add ability to create custom dataSets  
